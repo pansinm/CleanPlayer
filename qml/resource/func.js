0d7bd1b6b0f8822e14cf6520c640dc0e913113bb
panxin <pansinm@foxmail.com>
2018-07-26 13:16:24 +0800

    重新调整js代码格式

--- qml/resource/func.js
+++ qml/resource/func.js
@@ -1,31 +1,40 @@
 //克隆对象
-function objClone(obj){
-    var o;
-    switch(typeof obj){
-    case 'undefined': break;
-    case 'string'   : o = obj + '';break;
-    case 'number'   : o = obj - 0;break;
-    case 'boolean'  : o = obj;break;
-    case 'object'   :
-        if(obj === null){
-            o = null;
-        }else{
-            if(obj instanceof Array){
-                o = [];
-                for(var i = 0, len = obj.length; i < len; i++){
-                    o.push(objClone(obj[i]));
+function objClone(obj) {
+    var o
+    switch (typeof obj) {
+    case 'undefined':
+        break
+    case 'string':
+        o = obj + ''
+        break
+    case 'number':
+        o = obj - 0
+        break
+    case 'boolean':
+        o = obj
+        break
+    case 'object':
+        if (obj === null) {
+            o = null
+        } else {
+            if (obj instanceof Array) {
+                o = []
+                for (var i = 0, len = obj.length; i < len; i++) {
+                    o.push(objClone(obj[i]))
                 }
-            }else{
-                o = {};
-                for(var k in obj){
-                    o[k] = objClone(obj[k]);
+            } else {
+                o = {
+
+                }
+                for (var k in obj) {
+                    o[k] = objClone(obj[k])
                 }
             }
         }
-        break;
+        break
     default:
-        o = obj;break;
+        o = obj
+        break
     }
-    return o;
+    return o
 }
-
